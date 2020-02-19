40ba9d012adba58ac6b9671e423d66f7cd2b3b22
warzes <warzes@mail.ru>
2020-02-19 10:40:57 +1000

    -

---
+++ Code/Game/WinMain.cpp
@@ -0,0 +1,17 @@
+#pragma warning( push, 1 )
+#include <Windows.h>
+#pragma warning( pop )
+//-----------------------------------------------------------------------------
+#pragma comment(lib, "3rdparty.lib")
+#pragma comment(lib, "Core.lib")
+#pragma comment(lib, "Engine.lib")
+//-----------------------------------------------------------------------------
+int WINAPI wWinMain(
+	[[maybe_unused]] _In_ HINSTANCE hInstance,
+	[[maybe_unused]] _In_opt_ HINSTANCE hPrevInstance,
+	[[maybe_unused]] _In_ LPWSTR lpCmdLine,
+	[[maybe_unused]] _In_ int nCmdShow )
+{
+	return 0;
+}
+//-----------------------------------------------------------------------------
